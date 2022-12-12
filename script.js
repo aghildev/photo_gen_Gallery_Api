35fd94ae00647524186dbb83eadfbf2bf9349712
Aghil P Wilson <aghilpwilson1994@gmail.com>
2022-12-12 12:31:07 +0530

    code refractored

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function updateInput(e){
     searchValue = e.target.value
 }
 
-async function curatedPhotos(){
-    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1",{
+async function fetchApi(url) {
+    const dataFetch = await fetch(url,{
         method: "GET",
         headers: {
             Accept: "application/json",
@@ -26,7 +26,11 @@ async function curatedPhotos(){
         }
     }
     )
-    const data = await dataFetch.json()
+    const data = await dataFetch.json();
+    return data;
+}
+
+function generatePictures(data){
     data.photos.forEach(photo =>{
         const galleryImg = document.createElement("div");
         galleryImg.classList.add("gallery-img");
@@ -35,26 +39,18 @@ async function curatedPhotos(){
         `;
         gallery.appendChild(galleryImg);
     })
+}
+
+async function curatedPhotos(){
+   const data =await fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1")
+   generatePictures(data)
     
 }
 
 
 async function searchPhotos(query){
-    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${searchValue}&per_page=15&page=1`,{
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            Authorization:auth
-        }
-    })
-    const data = await dataFetch.json()
-    data.photos.forEach(photo =>{
-        const galleryImg = document.createElement("div");
-        galleryImg.classList.add("gallery-img");
-        galleryImg.innerHTML = `<img src = ${photo.src.large}></img>
-        <p>${photo.photographer}</p>
-        `;
-        gallery.appendChild(galleryImg);
-    })
+    const data =await fetchApi(`https://api.pexels.com/v1/search?query=${searchValue}&per_page=15&page=1`)
+  
+    generatePictures(data)
 }
 curatedPhotos()
